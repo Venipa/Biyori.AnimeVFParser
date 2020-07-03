86fd9d24f82f419d2e461300627c4bb8725d05a0
Venipa <admin@venipa.net>
2020-07-03 20:30:36 +0200

    Initial Commit

---
+++ AnimeVideoFilenameModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biyori.AnimeVFParser
+{
+    public class AnimeVideoFilenameModel
+    {
+        public string ReleaseGroup { get; set; }
+        public int ReleaseVersion { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public int Episode { get; set; }
+        public string EpisodeTitle { get; set; }
+        public VideoResolution Resolution { get; set; }
+        public string VideoTerm { get; set; }
+        public string AudioTerm { get; set; }
+        public string Checksum { get; set; }
+        public struct VideoResolution
+        {
+            int Width;
+            int Height;
+            public override string ToString()
+            {
+                return $"{Width}x{Height}";
+            }
+        }
+    }
+}
---
+++ AnimeVideoFilenameParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Biyori.AnimeVFParser
+{
+    public class AnimeVideoFilenameParser
+    {
+        public string Filename { get; private set; }
+        public AnimeVideoFilenameParser(string filename)
+        {
+            this.Filename = filename;
+        }
+        public void setFilename(string filename)
+        {
+            this.Filename = filename;
+        }
+        public AnimeVideoFilenameModel getInfo()
+        {
+            return new AnimeVideoFilenameModel()
+            {
+            };
+        }
+    }
+}
